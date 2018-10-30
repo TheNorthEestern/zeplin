4c9394b046a2c159ba54b393ebcd5ce9f5664c3c
Kacy James <inquen@gmail.com>
2018-10-29 22:51:18 -0400

    Added new files, removed unnecessary ones

---
+++ zeplin/Protocols/AchievementsViewInterface.swift
@@ -0,0 +1,13 @@
+//
+//  AchievementsViewInterface.swift
+//  zeplin
+//
+//  Created by Kacy James on 10/29/18.
+//  Copyright © 2018 Kacy James. All rights reserved.
+//
+
+import Foundation
+
+protocol AchievementsViewInterface: class {
+  func showAchievementData(achievements: [AchievementModel])
+}
---
+++ zeplin/View/VanillaNavigationControllerSubclass.swift
@@ -0,0 +1,13 @@
+//
+//  VanillaNavigationControllerSubclass.swift
+//  zeplin
+//
+//  Created by Kacy James on 10/28/18.
+//  Copyright © 2018 Kacy James. All rights reserved.
+//
+
+import UIKit
+
+class VanillaNavigationControllerSubclass: UINavigationController {
+  override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+}
